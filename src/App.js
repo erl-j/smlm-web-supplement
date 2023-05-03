69ff3912a360e51c343ab00f1c1aed5b23581d43
erl-j <nicolasjonason@gmail.com>
2023-05-04 01:16:21 +0900

    nicer layout

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
 import React, { useState } from 'react';
 
-sections = [
-  {name: "Dataset",
-  experiments: [
-    { "description": "Samples drawn from the augmented dataset", "id": "dataset" }
-  ]},
+const sections = [
   {
 
     name: "Unconditional generation",
     experiments: [
-      { "description": "These samples were generated without any constraints", "id": "unconstrained" }
+      { "description": "These samples were generated without any constraints.", "id": "unconstrained" }
     ],
   },
   {
     name: "Imputation",
     experiments: [
-      { "description": "Sequence imputation of an rectangular area of a piano roll. Generated with temperature = 0.75 and p=0.9.", "id": "imputation,_pitch_and_time" },
-      { "description": "Sequence imputation of an rectangular area in pitch-time space. Generated with temperature = 0.75 and p=0.9.", "id": "imputation,_time" },
-      { "description": "Generation of high pitches conditioned on low pitches", "id": "imputation,_pitch_upper" },
-      { "description": "Generation of low pitches conditioned on high pitches.", "id": "imputation,_pitch_lower" },
+      { "description": "Imputation. Generated with temperature = 0.75 and p = 0.9.", "id": "imputation,_time" },
+      { "description": "Imputation. Generated with temperature = 0.75 and p = 0.9.", "id": "imputation,_pitch_and_time" },
+      { "description": "Imputation of high pitches conditioned on low pitches", "id": "imputation,_pitch_upper" },
+      { "description": "Imputation of low pitches conditioned on high pitches.", "id": "imputation,_pitch_lower" },
     ],
   },
   {
@@ -46,32 +42,20 @@ sections = [
   {
     name: "Combining constraints",
     experiments: [
-      { "description": "Generation with multiple constraints. Pitch is constrained to major scale with root pitch 0, duration is set to be between 2 and 7, onsets are constrained to every 4 steps. Minimum number of notes is set to 16, maximum number of notes is set to 64. ", "id": "multiple_constraints,_major_scale,_onset_on_quarter_notes,_duration_is_4_or_8,_32_active_notes" },
+      {
+        "description": "Generation with multiple constraints. Pitch is constrained to major scale with root pitch 0, duration is set to be between 2 and 7, onsets are constrained to every 4 steps. Minimum number of notes is set to 16, maximum number of notes is set to 64. ",
+        "id": "multiple_constraints,_major_scale,_onset_on_quarter_notes,_duration_is_4_or_8,_32_active_notes"
+      },
     ],
-  }
+  },
+  {
+    name: "Dataset",
+    experiments: [
+      { "description": "Samples drawn from the augmented dataset.", "id": "dataset" }
+    ]
+  },
 ]
 
-const experiments = [
-  "unconstrained",
-  "imputation,_time",
-  "imputation,_pitch_lower",
-  "imputation,_pitch_and_time",
-  "imputation,_pitch_upper",
-  "pitch_constraint,_major_scale",
-  "pitch_constraint,_major_pentatonic_scale",
-  "duration_constraint,_<8",
-  "duration_constraint,_>8",
-  "onset_constraint,_4",
-  "onset_constraint,_8",
-  "onset_constraint,_16",
-  "multiple_constraints,_major_scale,_onset_on_quarter_notes,_duration_is_4_or_8,_32_active_notes",
-
-]
-
-const playAudio = (experiment, nr) => {
-  document.querySelectorAll("audio").forEach((audio) => audio.pause());
-  new Audio(`./supplement/audio/${experiment}_${nr}.wav`).play()
-}
 
 
 function App() {
@@ -79,21 +63,21 @@ function App() {
   const [selectedExample, setSelectedExample] = useState(null);
   const audioRef = React.useRef(null);
 
-  // on click this component stops all audio and plays an audio file. 
-  const img = (experiment, nr) => (
-    <div style={{
-      margin: 16,
-      border: selectedExample === experiment + "_" + nr ? "solid 4px green" : "solid 4px black",
-    }}
-      onClick={
-        () => {
-          setSelectedExample(experiment + "_" + nr);
+    // on click this component stops all audio and plays an audio file. 
+    const img = (experiment_id_nr) => (
+      <div style={{
+        border: selectedExample === experiment_id_nr ? "solid 5px aquamarine" : "solid 5px transparent",
+      }}
+        onClick={
+          () => {
+            setSelectedExample(experiment_id_nr);
+          }
         }
-      }
-    >
-      <img style={{ maxWidth: "100%", height: "auto" }} src={`./supplement/roll/${experiment}_${nr}.png`} />
-    </div>
-  )
+      >
+        <img style={{ maxWidth: "100%", height: "auto" }} src={`./supplement/roll/${experiment_id_nr}.png`} />
+      </div>
+    )
+  
 
   React.useEffect(() => {
     // stop all audio when a new example is selected
@@ -109,27 +93,35 @@ function App() {
       audioRef.current.play();
     }
 
+    console.log("selectedExample", selectedExample);
+
   }, [selectedExample])
 
   return (
-
-    <div>
+    <div style={{margin:4}}>
       <h1>Softly Masked Language Model - Web supplement</h1>
       <p>Click a piano roll to listen. The composition is played twice.</p>
-      {experiments.map((experiment) =>
-
-        <div style={{ display: "flex", flexDirection: "column", width: "70%", margin: "auto" }}>
-          <h1>{experiment}</h1>
-          <div style={{ display: "flex", flexDirection: "row", width: "100%" }}>
-            {img(experiment, 0)}
-            {img(experiment, 1)}
-          </div>
-          <div style={{ display: "flex", flexDirection: "row", width: "100%" }}>
-            {img(experiment, 2)}
-            {img(experiment, 3)}
-          </div>
-        </div>
-      )
+      {sections.map((section, idx) => {
+        let sectionName = section.name;
+        let experiments = section.experiments;
+        return  (
+        <div key={idx} style={{width: "70%", margin: "auto"}}>
+          <h1>{sectionName}</h1>
+          {experiments.map((experiment, experiment_idx) =>
+            <div key={experiment_idx} style={{ display: "flex", flexDirection: "column", "margin-bottom": 64  }}>
+              <p>{experiment.description}</p>
+              <div style={{ display: "flex", flexDirection: "row", width: "100%" }}>
+                {img(experiment.id+"_"+0)}
+                {img(experiment.id+"_"+1)}
+              </div>
+              <div style={{ display: "flex", flexDirection: "row", width: "100%" }}>
+                {img(experiment.id+"_"+2)}
+                {img(experiment.id+"_"+3)}
+              </div>
+            </div>
+          )}
+        </div>)
+      })
       }
     </div>
   );
